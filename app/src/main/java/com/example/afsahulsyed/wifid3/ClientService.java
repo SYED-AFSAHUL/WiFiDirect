08e14633429ed119a1f7a7c8f503e58f7249ac65
afsahulsyed <afsahulsyed@gmail.com>
2017-05-24 10:21:17 +0530

    added client and server class.

---
+++ app/src/main/java/com/example/afsahulsyed/wifid3/ClientService.java
@@ -0,0 +1,8 @@
+package com.example.afsahulsyed.wifid3;
+
+/**
+ * Created by afsahulsyed on 24/5/17.
+ */
+
+public class ClientService {
+}
---
+++ app/src/main/java/com/example/afsahulsyed/wifid3/ServerService.java
@@ -0,0 +1,10 @@
+package com.example.afsahulsyed.wifid3;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by afsahulsyed on 24/5/17.
+ */
+
+public class ServerService extends AsyncTask {
+}
